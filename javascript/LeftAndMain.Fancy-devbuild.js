09124db35c9818677213afc8242cda1347f3dafe
Gorrie <gorriecoe@gmail.com>
2016-01-21 09:02:33 +1300

    Fixed variables names

--- javascript/LeftAndMain.Fancy-devbuild.js
+++ javascript/LeftAndMain.Fancy-devbuild.js
@@ -1,15 +1,15 @@
 (function($) {
-    var $dev_trigger = $("#devbuild-trigger"),
+    var dev_trigger = "#devbuild-trigger",
         reset_time = 5000;
     // inject the link into the cms menu
-    $(".cms-menu-list").append('<li class="link devbuild"><a href="#" id="devbuild-trigger"><span class="icon icon-16 icon-help">&nbsp;</span><span class="text">Dev/Build</span></a></li>');
+    $(".cms-menu-list").append('<li class="link devbuild"><a id="devbuild-trigger"><span class="icon icon-16 icon-help">&nbsp;</span><span class="text">Dev/Build</span></a></li>');
 
     // look out for click
-    $dev_trigger.click(function(e) {
+    $(dev_trigger).click(function(e) {
 
         e.preventDefault();
 
-        $dev_trigger.removeClass("error")
+        $(dev_trigger).removeClass("error")
             .addClass("loading")
             .children(".text")
             .text("Building...");
@@ -20,19 +20,19 @@
         })
         .done(function( data, textStatus, xhr ) {
             // remove classes
-            $dev_trigger.removeClass("loading");
+            $(dev_trigger).removeClass("loading");
 
             // search for any errors from returned data
             if (data.search("ERROR") > 0) {
                 // change text to show an error has occured
-                $dev_trigger.attr('href', 'dev/build')
+                $(dev_trigger).attr('href', 'dev/build')
                     .attr('target', '_blank')
                     .addClass("error")
                     .children(".text")
                     .text("Build Failed");
                 // revert to default after 5 seconds
                 setTimeout(function(){
-                    $dev_trigger.attr('href', '#')
+                    $(dev_trigger).attr('href', '#')
                         .removeClass("error")
                         .children(".text")
                         .text("Dev/Build");
